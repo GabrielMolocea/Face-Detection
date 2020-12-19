81c0b72396b8ac5eb20e8e162c03a6cc35385a66
Molocea Gabriel <39118451+GabrielMolocea@users.noreply.github.com>
2020-12-19 14:22:05 +0200

    Add files via upload

---
+++ src/FaceDetection/Controller.java
@@ -0,0 +1,18 @@
+package FaceDetection;
+
+import javafx.fxml.*;
+import javafx.scene.control.*;
+import javafx.scene.image.*;
+
+
+
+public class Controller {
+    
+    @FXML
+    private Button cameraButton;
+    
+    @FXML
+    private ImageView cameraFrame;
+    
+    
+}

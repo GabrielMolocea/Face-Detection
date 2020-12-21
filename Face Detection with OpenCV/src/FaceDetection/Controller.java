b77300886d6ae7a88277dd77d9a132cd3ea67018
Gabriel Molocea <gabrielgabriel1235@gmail.com>
2020-12-21 15:48:09 +0200

    Update 3

--- Face Detection with OpenCV/src/FaceDetection/Controller.java
+++ Face Detection with OpenCV/src/FaceDetection/Controller.java
@@ -3,10 +3,17 @@ package FaceDetection;
 import javafx.fxml.*;
 import javafx.scene.control.*;
 import javafx.scene.image.*;
+import org.opencv.objdetect.*;
 import org.opencv.videoio.*;
 
+import java.util.*;
 import java.util.concurrent.*;
 
+    /**
+     * The controller associated with the only view of our application.
+     * The application logic is implemented here. It handles the button for starting/stopping the camera,
+     * the acquired video stream, the relative controls and the face detection/tracking.
+     */
 
 public class Controller {
     
@@ -41,6 +48,29 @@ public class Controller {
     // Checking if camera is active or disabled
     private boolean cameraActive;
     
-    //
+    // Face cascade classifier
+    private CascadeClassifier faceCascade;
+    private  int absoluteFaceSize;
+    
+    public int index = 0;
+    public int ind = 0;
+    
+    // New user String for training
+    public String newName;
+    
+    // HashMap form training sets
+    public HashMap<Integer, String> listOfNames = new HashMap<>();
+    
+    // Random number for training sets
+    private int random = (int) (Math.random() * 50 + 3);
+    
+    /**
+     *  Initializing init at the starting time
+     */
+    public void init() {
+        this.capture = new VideoCapture();
+        this.faceCascade = new CascadeClassifier();
+        this.absoluteFaceSize = 0;
+    }
     
 }
